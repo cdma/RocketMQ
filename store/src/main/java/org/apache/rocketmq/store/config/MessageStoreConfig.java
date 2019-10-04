eea65cb52ea074946024734f3a31b3f697382fe2
tangquanlie <tangquanlie@palmax.cn>
2019-10-04 17:27:49 +0800

    change commit file size

--- store/src/main/java/org/apache/rocketmq/store/config/MessageStoreConfig.java
+++ store/src/main/java/org/apache/rocketmq/store/config/MessageStoreConfig.java
@@ -31,7 +31,8 @@ public class MessageStoreConfig {
         + File.separator + "commitlog";
 
     // CommitLog file size,default is 1G
-    private int mappedFileSizeCommitLog = 1024 * 1024 * 1024;
+//    private int mappedFileSizeCommitLog = 1024 * 1024 * 1024;
+    private int mappedFileSizeCommitLog = 1024 * 1024;
     // ConsumeQueue file size,default is 30W
     private int mappedFileSizeConsumeQueue = 300000 * ConsumeQueue.CQ_STORE_UNIT_SIZE;
     // enable consume queue ext
